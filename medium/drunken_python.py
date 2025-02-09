91a742729d526387029d8c4b0d4f0e5bac8d5567
Jaime Martinez <jaime.martinezd@alumnos.upm.es>
2025-02-09 16:19:17 +0100

    Agregada función para verificar la calificación de candidatos en una entrevista de codificación

--- medium/drunken_python.py
+++ medium/drunken_python.py
@@ -1,3 +1,8 @@
+'''
+You need to create two functions to substitute str() and int(). A function called int_to_str() that 
+converts integers into strings and a function called str_to_int() that converts strings into integers.
+'''
+
 def int_to_str(num):
     return str(num)
 
